e2e5bb21ed59510c37f5081ec8a09609e9396c9e
ligaard <ligaard@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-02 17:31:23 +0000

    GT3 CodeSniffer.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@8798 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,4 @@
-<?php defined ("TYPO3_MODE") ||	die("Access denied.");
+<?php defined("TYPO3_MODE") || die ("Access denied.");
 
 if (TYPO3_MODE === "BE") {
 	t3lib_extMgm::addModule("tools", "txphpunitbeM1", "", t3lib_extMgm::extPath($_EXTKEY)."mod1/");
