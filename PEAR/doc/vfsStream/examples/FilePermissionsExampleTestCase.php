f09ddd3a823c0f067de5423bd8c600667c96d6a6
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-01-12 21:56:36 +0000

    Task #12145: Update PHPUnit to 3.5.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@42065 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ PEAR/doc/vfsStream/examples/FilePermissionsExampleTestCase.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Test for FilePermissionsExample.
+ *
+ * @package     bovigo_vfs
+ * @subpackage  examples
+ * @version     $Id: FilePermissionsExampleTestCase.php 203 2010-06-08 20:42:45Z google@frankkleine.de $
+ */
+require_once 'PHPUnit/Framework.php';
+require_once 'vfsStream/vfsStream.php';
+require_once 'FilePermissionsExample.php';
+/**
+ * Test for FilePermissionsExample.
+ *
+ * @package     bovigo_vfs
+ * @subpackage  examples
+ */
+class FilePermissionsExampleTestCase extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function directoryWritable()
+    {
+        vfsStream::setup('exampleDir');
+        $example = new FilePermissionsExample();
+        $example->writeConfig(array('foo' => 'bar'),
+                              vfsStream::url('exampleDir/writable.ini')
+        );
+
+        // assertions here
+    }
+
+    /**
+     * @test
+     */
+    public function directoryNotWritable()
+    {
+        vfsStream::setup('exampleDir', 0444);
+        $example = new FilePermissionsExample();
+        $example->writeConfig(array('foo' => 'bar'),
+                              vfsStream::url('exampleDir/notWritable.ini')
+        );
+    }
+}
+?>
