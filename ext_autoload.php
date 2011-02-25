52b8e0a8d6cb7f72dafb7e67d1734636cd2d832a
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-25 22:16:02 +0000

    Bug #13278: vfsStreamWrapper cannot be used if it is not included
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@44294 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -9,7 +9,15 @@ return array(
 	'tx_phpunit_testcase' => $extensionPath . 'Classes/TestCase.php',
 	'tx_phpunit_module1' => $extensionPath . 'mod1/class.tx_phpunit_module1.php',
 	'tx_phpunit_module1_ajax' => $extensionPath . 'mod1/class.tx_phpunit_module1_ajax.php',
-	'vfsstream' => $extensionPath . 'PEAR/vfsStream/vfsStream.php',
 	'tx_phpunit_test_testsuite' => $extensionPath . 'Tests/tx_phpunit_testsuite.php',
+	'vfsstream' => $extensionPath . 'PEAR/vfsStream/vfsStream.php',
+	'vfsstreamabstractContent' => $extensionPath . 'PEAR/vfsStream/vfsStreamAbstractContent.php',
+	'vfsstreamcontainer' => $extensionPath . 'PEAR/vfsStream/vfsStreamContainer.php',
+	'vfsstreamcontaineriterator' => $extensionPath . 'PEAR/vfsStream/vfsStreamContainerIterator.php',
+	'vfsstreamcontent' => $extensionPath . 'PEAR/vfsStream/vfsStreamContent.php',
+	'vfsstreamdirectory' => $extensionPath . 'PEAR/vfsStream/vfsStreamDirectory.php',
+	'vfsstreamexception' => $extensionPath . 'PEAR/vfsStream/vfsStreamException.php',
+	'vfsstreamfile' => $extensionPath . 'PEAR/vfsStream/vfsStreamFile.php',
+	'vfsstreamwrapper' => $extensionPath . 'PEAR/vfsStream/vfsStreamWrapper.php',
 );
 ?>
