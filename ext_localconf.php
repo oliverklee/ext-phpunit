6039ae4948ef3c574876d6f836e58ff6c4a8272c
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-28 06:16:08 +0000

    Bug #13427: Use $GLOBALS['TYPO3_CONF_VARS'] instead of $TYPO3_CONF_VARS
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@44337 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,10 +3,10 @@ if (!defined('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
 
-if (is_dir($TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['phpunitlib'])) {
-	$phpunitlib = $TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['phpunitlib'] . PATH_SEPARATOR;
+if (is_dir($GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['phpunitlib'])) {
+	$phpunitlib = $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY]['phpunitlib'] . PATH_SEPARATOR;
 } else {
 	$phpunitlib = t3lib_extMgm::extPath('phpunit') . 'PEAR/';
 }
@@ -14,10 +14,10 @@ if (is_dir($TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['phpunitlib'])) {
 define(TX_PHPUNITLIB_EXTPATH, $phpunitlib);
 set_include_path(TX_PHPUNITLIB_EXTPATH . PATH_SEPARATOR . get_include_path());
 
-$TYPO3_CONF_VARS['BE']['AJAX']['tx_phpunit_module1_ajax'] = 'typo3conf/ext/phpunit/mod1/class.tx_phpunit_module1_ajax.php:tx_phpunit_module1_ajax->ajaxBroker';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['AJAX']['tx_phpunit_module1_ajax'] = 'typo3conf/ext/phpunit/mod1/class.tx_phpunit_module1_ajax.php:tx_phpunit_module1_ajax->ajaxBroker';
 
 if (TYPO3_MODE === 'BE') {
-	$TYPO3_CONF_VARS['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array(
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array(
 		'EXT:' . $_EXTKEY . '/Classes/Cli/TestRunner.php',
 		'_CLI_phpunit'
 	);
