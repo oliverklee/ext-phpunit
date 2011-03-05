e465c461031ac4d214dee75334e8640993a63a4a
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-03-05 11:33:02 +0000

    Clean up the autoload file a bit by putting the vfsStream directory in a local variable.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@44665 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_autoload.php
+++ ext_autoload.php
@@ -1,5 +1,6 @@
 <?php
 $extensionPath = t3lib_extMgm::extPath('phpunit');
+$vfsStreamPath = $extensionPath . 'PEAR/vfsStream/';
 return array(
 	'tx_phpunit_backend_ajax' => $extensionPath . 'Classes/BackEnd/Ajax.php',
 	'tx_phpunit_backend_module' => $extensionPath . 'Classes/BackEnd/Module.php',
@@ -17,14 +18,14 @@ return array(
 	'tx_phpunit_testablecode' => $extensionPath . 'Classes/TestableCode.php',
 	'tx_phpunit_testcase' => $extensionPath . 'Classes/TestCase.php',
 	'tx_phpunit_test_testsuite' => $extensionPath . 'Tests/tx_phpunit_testsuite.php',
-	'vfsstream' => $extensionPath . 'PEAR/vfsStream/vfsStream.php',
-	'vfsstreamabstractContent' => $extensionPath . 'PEAR/vfsStream/vfsStreamAbstractContent.php',
-	'vfsstreamcontainer' => $extensionPath . 'PEAR/vfsStream/vfsStreamContainer.php',
-	'vfsstreamcontaineriterator' => $extensionPath . 'PEAR/vfsStream/vfsStreamContainerIterator.php',
-	'vfsstreamcontent' => $extensionPath . 'PEAR/vfsStream/vfsStreamContent.php',
-	'vfsstreamdirectory' => $extensionPath . 'PEAR/vfsStream/vfsStreamDirectory.php',
-	'vfsstreamexception' => $extensionPath . 'PEAR/vfsStream/vfsStreamException.php',
-	'vfsstreamfile' => $extensionPath . 'PEAR/vfsStream/vfsStreamFile.php',
-	'vfsstreamwrapper' => $extensionPath . 'PEAR/vfsStream/vfsStreamWrapper.php',
+	'vfsstream' => $vfsStreamPath . 'vfsStream.php',
+	'vfsstreamabstractContent' => $vfsStreamPath . 'vfsStreamAbstractContent.php',
+	'vfsstreamcontainer' => $vfsStreamPath . 'vfsStreamContainer.php',
+	'vfsstreamcontaineriterator' => $vfsStreamPath . 'vfsStreamContainerIterator.php',
+	'vfsstreamcontent' => $vfsStreamPath . 'vfsStreamContent.php',
+	'vfsstreamdirectory' => $vfsStreamPath . 'vfsStreamDirectory.php',
+	'vfsstreamexception' => $vfsStreamPath . 'vfsStreamException.php',
+	'vfsstreamfile' => $vfsStreamPath . 'vfsStreamFile.php',
+	'vfsstreamwrapper' => $vfsStreamPath . 'vfsStreamWrapper.php',
 );
 ?>
