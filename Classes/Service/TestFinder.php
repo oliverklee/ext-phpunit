a761c1803248ed3b854bdc23685609a309f3946a
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-03-29 19:03:29 +0000

    Fixed Bug #33820: Fix all deprecation log warnings
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@60088 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Service/TestFinder.php
+++ Classes/Service/TestFinder.php
@@ -325,15 +325,18 @@ class Tx_Phpunit_Service_TestFinder implements t3lib_Singleton {
 	 * @return array<string> the keys of the loaded extensions, might be empty
 	 */
 	protected function getLoadedExtensionKeys() {
-		$loadedExtensionList = isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'])
-			? $GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'] : '';
-		if (t3lib_div::int_from_ver(TYPO3_version) >= 4005000) {
+		$version = class_exists('t3lib_utility_VersionNumber')
+			? t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version)
+			: t3lib_div::int_from_ver(TYPO3_version);
+		if ($version >= 4005000) {
 			$requiredExtensionList = t3lib_extMgm::getRequiredExtensionList();
 		} else {
 			$requiredExtensionList = isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['requiredExt'])
 				? $GLOBALS['TYPO3_CONF_VARS']['EXT']['requiredExt'] : '';
 		}
 
+		$loadedExtensionList = isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'])
+			? $GLOBALS['TYPO3_CONF_VARS']['EXT']['extList'] : '';
 		$allExtensionKeys = t3lib_div::trimExplode(',', $loadedExtensionList . ',' . $requiredExtensionList, TRUE);
 
 		return array_unique($allExtensionKeys);
