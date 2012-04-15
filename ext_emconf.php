9a3ef5d36b0dc8a0f88a2a2f768afe38d225755c
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-15 08:07:34 +0000

    Fixed Bug #36092: Require PHP >= 5.2.7 and recommend PHP >= 5.3.9
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpunit/trunk@60731 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.3-0.0.0',
+			'php' => '5.2.7-0.0.0',
 			'typo3' => '4.5.0-0.0.0',
 		),
 		'conflicts' => array(
