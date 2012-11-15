f5d6e3ae6a87c547df06cf3b44f79b7ae9375322
Oliver Klee <typo3-coding@oliverklee.de>
2012-11-15 17:25:15 +0100

    [TASK] Require PHP >= 5.3
    Now that RHEL provides PHP 5.3, we have decided to drop support
    for PHP 5.2.x and require PHP >= 5.3.
    
    Resolves: #36082
    
    Change-Id: Ib4abc731df29b1035d732b42949f20954a5b5ff5

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.7-0.0.0',
+			'php' => '5.3.0-0.0.0',
 			'typo3' => '4.5.0-6.0.99',
 		),
 		'conflicts' => array(
