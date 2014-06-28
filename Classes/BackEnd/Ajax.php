8cf2087a6c22f802805bbece8f6d59bf934a447f
Oliver Klee <typo3-coding@oliverklee.de>
2014-06-28 16:12:38 +0200

    [TASK] Update the license headers
    This change updates all PHP file license headers to the new
    and shorter version used in TYPO3 CMS. It also adds a
    license.txt.
    
    Resolves: #59961
    Change-Id: I0578c313a8ecbdee144523a806630bacf93d4965
    Reviewed-on: https://review.typo3.org/31190
    Reviewed-by: Philipp Gampe
    Tested-by: Philipp Gampe

--- Classes/BackEnd/Ajax.php
+++ Classes/BackEnd/Ajax.php
@@ -1,29 +1,19 @@
 <?php
-/***************************************************************
- * Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- * (c) 2008-2013 Kasper Ligaard <kasperligaard@gmail.com>
- * All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- * This script is part of the TYPO3 project. The TYPO3 project is
- * free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- * The GNU General Public License can be found at
- * http://www.gnu.org/copyleft/gpl.html.
- *
- * This script is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
- * This class uses the new AJAX broker in Typo3 4.2.
+ * This class uses the new AJAX broker in TYPO3 4.2.
  *
  * @see http://bugs.typo3.org/view.php?id=7096
  *
