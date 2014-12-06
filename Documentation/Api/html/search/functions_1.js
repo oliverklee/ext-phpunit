c19b9dcdc97a4af8c0f97bddb46010bab0d30088
Oliver Klee <typo3-coding@oliverklee.de>
2014-12-06 20:07:36 +0100

    [TASK] Update the API documentation
    Resolves: #63633
    
    Change-Id: I9b72457bf5bec9c4fbe94bf547672ecd9ef8d3bf
    Reviewed-on: http://review.typo3.org/35114
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

---
+++ Documentation/Api/html/search/functions_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['addadditionalheaderdata',['addAdditionalHeaderData',['../classTx__Phpunit__BackEnd__Module.html#a1a3bf46341826a4be7b947e65371e69a',1,'Tx_Phpunit_BackEnd_Module']]],
+  ['addriskytest',['addRiskyTest',['../classTx__Phpunit__BackEnd__TestListener.html#a5141e27357dcfe0cd8b049c91143ed39',1,'Tx_Phpunit_BackEnd_TestListener']]],
+  ['addtodummyfilelist',['addToDummyFileList',['../classTx__Phpunit__Framework.html#a0c731de916ac52c0a80fe9c70386a92d',1,'Tx_Phpunit_Framework']]],
+  ['ajaxbroker',['ajaxBroker',['../classTx__Phpunit__BackEnd__Ajax.html#a41d2920d5b29f1fb8cc9defa9805ff4b',1,'Tx_Phpunit_BackEnd_Ajax']]],
+  ['assertioncount',['assertionCount',['../classTx__Phpunit__BackEnd__TestListener.html#acfb448fc603321cba5cfe5fa40aefc58',1,'Tx_Phpunit_BackEnd_TestListener']]]
+];
