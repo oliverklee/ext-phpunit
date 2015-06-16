d867c7cbff699ef57ce36725b4c0815612c325fc
Oliver Klee <typo3-coding@oliverklee.de>
2015-06-16 16:16:47 +0200

    [TASK] Set compatibility version to 7.4
    Resolves: #67505
    Change-Id: I5b54e4ee2b48dfbbd83aee21f4f13f3244558a20
    Reviewed-on: http://review.typo3.org/40302
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.4.0-5.6.99',
-			'typo3' => '6.2.4-7.1.99',
+			'typo3' => '6.2.4-7.4.99',
 		),
 		'conflicts' => array(
 		),
