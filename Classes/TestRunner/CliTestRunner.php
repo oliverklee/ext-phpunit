250fe2d8c60b5ff45d578806bb17e494e2a7f57b
Nicole Cordes <typo3@cordes.co>
2015-11-27 23:27:37 +0100

    [BUGFIX] Remove Closures from TYPO3_CONF_VARS
    Closures cannot be serialized and need to be removed before running the
    tests and be restored afterwards.
    
    Resolves: #71909
    Change-Id: Ia947657cdd83844563f81e560d6e9e2e198392db
    Reviewed-on: https://review.typo3.org/45006
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- Classes/TestRunner/CliTestRunner.php
+++ Classes/TestRunner/CliTestRunner.php
@@ -27,8 +27,40 @@ class Tx_Phpunit_TestRunner_CliTestRunner extends Tx_Phpunit_TestRunner_Abstract
 	 * @return void
 	 */
 	public function run() {
+		// Store current TYPO3 configuration and set the default one
+		// This is needed as the configuration might include closures which cannot be backed up
+		$globalBackup = $this->removeClosures($GLOBALS['TYPO3_CONF_VARS']);
+
+		// Run unit tests
 		/** @var string */
 		define('PHPUnit_MAIN_METHOD', 'PHPUnit_TextUI_Command::main');
 		PHPUnit_TextUI_Command::main();
+
+		// Restore configuration
+		$GLOBALS['TYPO3_CONF_VARS'] = array_merge($GLOBALS['TYPO3_CONF_VARS'], $globalBackup);
+	}
+
+	/**
+	 * @param array $variables
+	 * @return array
+	 */
+	protected function removeClosures(array &$variables) {
+		$backup = array();
+		foreach ($variables as $key => &$value) {
+			if (!is_array($value) && !$value instanceof Closure) {
+				continue;
+			}
+			if (is_array($value) && !empty($value)) {
+				$valueBackup = $this->removeClosures($value);
+				if (!empty($valueBackup)) {
+					$backup[$key] = $valueBackup;
+				}
+			} elseif ($value instanceof Closure) {
+				$backup[$key] = $value;
+				unset($variables[$key]);
+			}
+		}
+
+		return $backup;
 	}
 }
