9d11869e6e541eee82d8697e1fd21278d0046822
Nicole Cordes <typo3@cordes.co>
2015-11-28 23:11:08 +0100

    [BUGFIX] Ensure Tests classes are available
    With patch https://review.typo3.org/45004/ class information from
    Tests folder is needed as well, so the folder needs to be included in
    the autoload part of the ext_emconf.php
    
    Resolves: #71941
    Change-Id: Ic77a663896fbe5473b2e3c66b6acd06ba08e490b
    Reviewed-on: https://review.typo3.org/45025
    Reviewed-by: Helmut Hummel <helmut.hummel@typo3.org>
    Tested-by: Helmut Hummel <helmut.hummel@typo3.org>
    Reviewed-by: Nicole Cordes <typo3@cordes.co>
    Tested-by: Nicole Cordes <typo3@cordes.co>

--- ext_emconf.php
+++ ext_emconf.php
@@ -45,6 +45,7 @@ $EM_CONF[$_EXTKEY] = array(
     'autoload' => array(
         'classmap' => array(
             'Classes',
+            'Tests',
         ),
     ),
     '_md5_values_when_last_written' => '',
