855ff2332ded414380f15d73f3220e357cc243d4
Felix Rauch <rauch@skaiamail.de>
2015-11-28 16:16:45 +0100

    [FEATURE] Add a CheckboxViewHelper
    This view helper is not used yet, but will be used in #36174 (SettingsViewHelper).
    
    Change-Id: Ie881e3a02d52040f7c9b5b9788835970914fe8a0
    Resolves: #71920
    Reviewed-on: https://review.typo3.org/45016
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

---
+++ Classes/ViewHelpers/CheckboxViewHelper.php
@@ -0,0 +1,126 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * This view helper renders an input field of type checkbox.
+ *
+ * @package TYPO3
+ * @subpackage tx_phpunit
+ *
+ * @author Felix Rauch <rauch@skaiamail.de>
+ */
+class Tx_Phpunit_ViewHelpers_CheckboxViewHelper extends Tx_Phpunit_ViewHelpers_AbstractTagViewHelper
+{
+    /**
+     * @var string
+     */
+    protected $tagName = 'input';
+
+    /**
+     * Any additional attributes can be set here. They are resolved as key="value" in the resulting tag.
+     *
+     * Note: The keys "type" and "value" are reserved and will be overridden by the ViewHelper properties.
+     *
+     * @var string[]|int[]
+     */
+    protected $additionalAttributes = array();
+
+    /**
+     * @var string
+     */
+    protected $type = 'checkbox';
+
+    /**
+     * According to HTML spec, an <input> element of type "checkbox" must have a value attribute (even if it is empty).
+     *
+     * @var string
+     */
+    protected $value = '';
+
+    /**
+     * Tx_Phpunit_ViewHelpers_CheckboxViewHelper constructor.
+     *
+     * @param string $value
+     * @param string[]|int[] $additionalAttributes
+     */
+    public function __construct($value = '', array $additionalAttributes = array())
+    {
+        $this->value = $value;
+        $this->additionalAttributes = $additionalAttributes;
+    }
+
+    /**
+     * Renders the input field with the set attributes and value
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $attributes = array_merge(
+            $this->additionalAttributes,
+            array(
+                'type' => $this->type,
+                'value' => $this->value,
+            )
+        );
+
+        return $this->renderTag(
+            $this->tagName,
+            $attributes
+        );
+    }
+
+    /**
+     * @return string[]|int[]
+     */
+    public function getAdditionalAttributes()
+    {
+        return $this->additionalAttributes;
+    }
+
+    /**
+     * @param string[]|int[] $additionalAttributes
+     */
+    public function setAdditionalAttributes(array $additionalAttributes)
+    {
+        $this->additionalAttributes = $additionalAttributes;
+    }
+
+    /**
+     * Adds the given array of additional attributes to the existing additional attributes.
+     * If keys are duplicated, this function will override the existing key.
+     *
+     * @param string[]|int[] $additionalAttributes
+     */
+    public function addAdditionalAttributes(array $additionalAttributes)
+    {
+        $this->additionalAttributes = array_merge($this->additionalAttributes, $additionalAttributes);
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+}
---
+++ Tests/Unit/ViewHelpers/CheckboxViewHelperTest.php
@@ -0,0 +1,202 @@
+<?php
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+/**
+ * Test case for Tx_Phpunit_ViewHelpers_CheckboxViewHelper.
+ *
+ * @package TYPO3
+ * @subpackage tx_phpunit
+ *
+ * @author Felix Rauch <rauch@skaiamail.de>
+ */
+class Tx_Phpunit_Tests_Unit_ViewHelpers_CheckboxViewHelperTest extends Tx_Phpunit_TestCase
+{
+    /**
+     * @test
+     */
+    public function classIsSubclassOfAbstractTagViewHelper()
+    {
+        self::assertInstanceOf(
+            'Tx_Phpunit_ViewHelpers_AbstractTagViewHelper',
+            new Tx_Phpunit_ViewHelpers_CheckboxViewHelper()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function viewHelperRendersAnInputTagOfTypeCheckbox()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper();
+
+        self::assertRegExp(
+            '/^<input[^$]+type="checkbox"[^$]*\/>$/',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function viewHelperRendersAnInputTagWithValueGivenInConstructor()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper('test');
+
+        self::assertRegExp(
+            '/^<input[^$]+value="test"[^$]*\/>$/',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function viewHelperRendersAnInputTagWithAdditionalAttributesGivenInConstructor()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper(
+            '',
+            array(
+                'foo' => 'bar'
+            )
+        );
+
+        self::assertRegExp(
+            '/^<input[^$]+foo="bar"[^$]*\/>$/',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function viewHelperRendersAnInputTagWithAdditionalAttributesAddedAfterInstantiation()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper(
+            '',
+            array(
+                'some' => 'attribute'
+            )
+        );
+
+        $subject->addAdditionalAttributes(
+            array(
+                'foo' => 'bar'
+            )
+        );
+
+        self::assertRegExp(
+            '/^<input[^$]+foo="bar"[^$]*\/>$/',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function reservedAttributeTypeIsPreserved()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper();
+
+        // This should have no effect on the rendered tag:
+        $subject->setAdditionalAttributes(
+            array(
+                'type' => 'will be overridden'
+            )
+        );
+
+        self::assertRegExp(
+            '/^<input[^$]+type="checkbox"[^$]*\/>$/',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function reservedAttributeValueIsPreserved()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper('test');
+
+        // This should have no effect on the rendered tag:
+        $subject->setAdditionalAttributes(
+            array(
+                'value' => 'will be overridden'
+            )
+        );
+
+        self::assertRegExp(
+            '/^<input[^$]+value="test"[^$]*\/>$/',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addingReservedAttributeToAdditionalAttributesDoesNotGenerateDuplicateAttributes()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper('test');
+
+        $subject->setAdditionalAttributes(
+            array(
+                'value' => 'another'
+            )
+        );
+
+        // Only one match should be found
+        self::assertSame(
+            1,
+            preg_match_all('/value="test"|value="another"/', $subject->render())
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function addingAdditionalAttributesWillPreserveExistingAdditionalAttributesWithDifferentKeys()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper(
+            '',
+            array(
+                'mustBe' => 'preserved'
+            )
+        );
+
+        $subject->addAdditionalAttributes(
+            array(
+                'foo' => 'bar'
+            )
+        );
+
+        self::assertContains(
+            'mustBe="preserved"',
+            $subject->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function changingTheValueAfterInstantiationIsPossible()
+    {
+        $subject = new Tx_Phpunit_ViewHelpers_CheckboxViewHelper('oldValue');
+
+        $subject->setValue('newValue');
+
+        self::assertContains(
+            'value="newValue"',
+            $subject->render()
+        );
+    }
+}
