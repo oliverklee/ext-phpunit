681b486617e874425a15c943d4a2d343a57967a7
Oliver Klee <typo3-coding@oliverklee.de>
2015-12-08 23:23:56 +0100

    [TASK] Increase maximum allowed TYPO3 and PHP versions
    Now PHP up to 7.0.99 and TYPO3 up to 7.6.99 are allowed.
    
    Resolves: #72115
    
    Change-Id: I2dbaddb60cbede2d1a31e1e015a38cbede15bf99
    Reviewed-on: https://review.typo3.org/45190
    Reviewed-by: Richard Haeser <richardhaeser@gmail.com>
    Tested-by: Richard Haeser <richardhaeser@gmail.com>
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,8 +35,8 @@ $EM_CONF[$_EXTKEY] = array(
     'CGLcompliance_note' => '',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.4.0-5.6.99',
-            'typo3' => '6.2.4-7.4.99',
+            'php' => '5.4.0-7.0.99',
+            'typo3' => '6.2.4-7.6.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
