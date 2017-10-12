b8ecf4affbc320556c78500951121114dfcaf2d5
Oliver Klee <typo3-coding@oliverklee.de>
2017-10-12 20:37:56 +0200

    [BUGFIX] Allow the CLI test runner in TYPO3 CMS >= 8.7
    Resolves: #82753
    
    Change-Id: I281d510ecc5b55729a8a96c75bedcbfd8237b012
    Reviewed-on: https://review.typo3.org/54388
    Reviewed-by: Oliver Klee <typo3-coding@oliverklee.de>
    Tested-by: Oliver Klee <typo3-coding@oliverklee.de>

--- Classes/TestRunner/AbstractCliTestRunner.php
+++ Classes/TestRunner/AbstractCliTestRunner.php
@@ -14,7 +14,8 @@
 
 use TYPO3\CMS\Core\Controller\CommandLineController;
 
-defined('TYPO3_cliMode') or die('Access denied: CLI only.');
+// The first check is for TYPO3 CMS <= 7.6, while the second is for TYPO3 CMS >= 8.7.
+(defined('TYPO3_cliMode') || (TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_CLI)) or die('Access denied: CLI only.');
 
 /**
  * Abstract TestRunner class. Can be used to implement other TestRunners which need CLI scope.
