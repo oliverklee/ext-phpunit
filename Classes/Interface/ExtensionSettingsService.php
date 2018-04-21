be807ea294e57a58aa31f8531137c0c91254fec8
Oliver Klee <typo3-coding@oliverklee.de>
2018-04-21 12:22:23 +0200

    [CLEANUP] Drop the incorrect TYPO3 Core license headers

--- Classes/Interface/ExtensionSettingsService.php
+++ Classes/Interface/ExtensionSettingsService.php
@@ -1,16 +1,4 @@
 <?php
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
 
 /**
  * This interface provides functions for reading the settings of the PHPUnit extension (as set in the extension manager).
--- Classes/ViewHelpers/AbstractTagViewHelper.php
+++ Classes/ViewHelpers/AbstractTagViewHelper.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
- * This file is part of the TYPO3 CMS project.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
-
 /**
  * Tag based view helper.
  * Should be used as the base class for all view helpers which output simple tags, as it provides some useful
