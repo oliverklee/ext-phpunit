c7b7d28da842ae1445b2b8a9f590c45fd4086f97
Oliver Klee <github@oliverklee.de>
2018-04-28 15:49:59 +0200

    [TASK] Drop support for PHP 5.5 (#37)

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints' => [
         'depends' => [
-            'php' => '5.5.0-7.0.99',
+            'php' => '5.6.0-7.0.99',
             'typo3' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
