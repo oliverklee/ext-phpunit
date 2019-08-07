0153c0977c1c27a58327086497efdc0f5acd5e46
Oliver Klee <typo3-coding@oliverklee.de>
2019-08-07 18:24:50 +0200

    [CLEANUP] Streamline ext_localconf.php and ext_tables.php

--- ext_tables.php
+++ ext_tables.php
@@ -9,4 +9,17 @@ if (TYPO3_MODE === 'BE'
         '',
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('phpunit') . 'Classes/BackEnd/'
     );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerAjaxHandler(
+        'PHPUnitAJAX::saveCheckbox',
+        'Tx_Phpunit_BackEnd_Ajax->ajaxBroker'
+    );
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys']['phpunit'] = [
+    'EXT:phpunit/Scripts/ManualCliTestRunner.php',
+    '_CLI_phpunit',
+];
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys']['phpunit_ide_testrunner'] = [
+    'EXT:phpunit/Scripts/IdeTestRunner.php',
+    '_CLI_phpunit',
+];
