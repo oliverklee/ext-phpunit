2b6a8931d9c7a69c13571e21083d5f060c8faca7
Oliver Klee <typo3-coding@oliverklee.de>
2020-11-24 20:05:36 +0100

    [TASK] Prepare the 6.5.16 release (#204)

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'PHPUnit',
     'description' => 'Unit testing for TYPO3. Includes PHPUnit and a CLI test runner.',
-    'version' => '6.5.15',
+    'version' => '6.5.16',
     'category' => 'misc',
     'constraints' => [
         'depends' => [
