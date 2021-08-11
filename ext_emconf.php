38ab4b2c3441262c49d92b0b1bbe89d881d047d9
Oliver Klee <typo3-coding@oliverklee.de>
2021-08-11 23:29:24 +0200

    [FEATURE] Allow installations with PHP 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,7 +7,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'misc',
     'constraints' => [
         'depends' => [
-            'php' => '7.2.0-7.4.99',
+            'php' => '7.2.0-',
             'typo3' => '9.5.0-10.4.99',
         ],
         'conflicts' => [],
