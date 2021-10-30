dbd49c935663b19ba80569291de92a579ae20ecb
Oliver Klee <typo3-coding@oliverklee.de>
2021-10-30 18:40:56 +0200

    [TASK] Remove the non-namespaced class aliases

--- Migrations/Code/ClassAliasMap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'Tx_Phpunit_Interface_AccessibleObject' => \OliverKlee\PhpUnit\Interfaces\AccessibleObject::class,
-    'Tx_Phpunit_TestCase' => \OliverKlee\PhpUnit\TestCase::class,
-];
