dfdd1a0c6b58850a23b669bcd86557d70afba0ca
Oliver Klee <github@oliverklee.de>
2021-10-30 19:07:29 +0200

    [CLEANUP] Drop `@author` annotations (#290)
    We have git now.

--- Classes/TestCase.php
+++ Classes/TestCase.php
@@ -8,13 +8,6 @@ namespace OliverKlee\PhpUnit;
  * Base class for test cases.
  *
  * @deprecated will be removed in phpunit 9.x; directly use `\PHPUnit\Framework\TestCase` instead
- *
- * @author Robert Lemke <robert@typo3.org>
- * @author Kasper Ligaard <kasperligaard@gmail.com>
- * @author Soren Soltveit <sso@systime.dk>
- * @author Michael Klapper <michael.klapper@aoemedia.de>
- * @author Oliver Klee <typo3-coding@oliverklee.de>
- * @author Nicole Cordes <nicole.cordes@googlemail.com>
  */
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
